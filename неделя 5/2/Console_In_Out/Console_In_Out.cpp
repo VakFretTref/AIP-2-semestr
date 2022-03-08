ede936f5219dee7c34c1b45f761f9fa7848bdffe
VakFretTref <93794794+VakFretTref@users.noreply.github.com>
2022-03-08 17:15:39 +0300

    09.03

---
+++ неделя 5/2/Console_In_Out/Console_In_Out.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+//I
+int main()
+{
+    int x;
+
+    cout << "enter x : ";
+    cin >> x;
+    cout << "y = ";
+
+    if (x > 7) 
+    {
+        cout << 2 * pow(x, 2) - 3 << endl;
+    }
+    else if (x < 7) 
+    {
+        cout << 2 * abs(x) + 3 << endl;
+    }
+    else cout << 0 << endl;
+
+    return 0;
+}
---
+++ неделя 6/4/Console_In_Out/Console_In_Out.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+//г
+int main()
+{
+    int x, y;
+
+    cout << "Enter x : ";
+    cin >> x;
+
+    cout << "Enter y : ";
+    cin >> y;
+
+    if ((y - 2 > x * x) && (y < x) || (y < -x)) cout << "Hits" << endl;
+    else cout << "Misses" << endl;
+
+    return 0;
+}
