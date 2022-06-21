a9ad1ccdbc17e2a4b4700f6641a53823aa575fa8
NikitaPerepelov <vakfret@gmail.com>
2022-06-21 12:37:18 +0300

    .

---
+++ неделя 15 - 16/1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <array>
+//2
+
+void multiplication(std::array <std::array<int, 3>, 3>& M)
+{
+    int b;
+
+    for (int i = 0; i < 3; i++)
+    {
+        b = M[i][1];
+
+        for (int j = 0; j < 3; j++)
+            M[i][j] *= b;
+    }
+}
+
+void outputMatrix(std::array <std::array<int, 3>, 3> M)
+{
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            std::cout << M[i][j] << ' ';
+        }
+        std::cout << std::endl;
+    }
+}
+
+int main()
+{
+    
+    std::array <std::array<int, 3>, 3> M;
+
+    for (int i = 0; i < 3; i++)
+        for (int j = 0; j < 3; j++)
+            M[i][j] = rand() % 21 - 10;
+
+    std::cout << "matrix before: \n" << std::endl;
+
+    outputMatrix(M);
+
+    std::cout << "\nmatrix after: \n" << std::endl;
+
+    multiplication(M);
+    outputMatrix(M);
+
+    getchar();
+
+    return 0;
+}
---
+++ неделя 15 - 16/2/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <array>
+//4
+
+void CreatingNewMatrix(std::array <std::array<int, 3>, 3>& M)
+{
+    int sum = 0;
+
+    for (int i = 0; i < 3; i++)
+        for (int j = i; j < 3; j += 2)
+            if (M[i][j] > 0) 
+                sum += M[i][j];
+}
+
+void outputMatrix(std::array <std::array<int, 3>, 3> M)
+{
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j ++)
+        {
+            std::cout << M[i][j] << ' ';
+        }
+        std::cout << std::endl;
+    }
+}
+
+int main()
+{
+    
+    std::array <std::array<int, 3>, 3> M;
+
+    for (int i = 0; i < 3; i++)
+        for (int j = 0; j < 3; j++)
+            M[i][j] = rand() % 21 - 10;
+
+    std::cout << "matrix before: \n" << std::endl;
+
+    outputMatrix(M);
+
+    std::cout << "\nmatrix after: \n" << std::endl;
+
+    CreatingNewMatrix(M);
+    outputMatrix(M);
+
+    getchar();
+
+    return 0;
+}
