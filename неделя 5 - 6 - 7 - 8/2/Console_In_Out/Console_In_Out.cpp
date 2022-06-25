c4137cc291c28419cacfbb0a02488f460c5467f2
VakFretTref <93794794+VakFretTref@users.noreply.github.com>
2022-06-25 16:40:26 +0300

    Update Console_In_Out.cpp

--- неделя 5 - 6 - 7 - 8/2/Console_In_Out/Console_In_Out.cpp
+++ неделя 5 - 6 - 7 - 8/2/Console_In_Out/Console_In_Out.cpp
@@ -7,17 +7,15 @@ int main()
     int x;
 
     cout << "enter x : ";
+    
     cin >> x;
+    
     cout << "y = ";
 
     if (x > 7) 
-    {
         cout << 2 * pow(x, 2) - 3 << endl;
-    }
     else if (x < 7) 
-    {
         cout << 2 * abs(x) + 3 << endl;
-    }
     else cout << 0 << endl;
 
     return 0;
