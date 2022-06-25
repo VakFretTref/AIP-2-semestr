8c5fa0563a5edc9e72a59bcb841f578e8cf36490
VakFretTref <93794794+VakFretTref@users.noreply.github.com>
2022-06-25 16:13:00 +0300

    .

--- неделя 1/2/Console_In_Out/Console_In_Out.cpp
+++ неделя 1/2/Console_In_Out/Console_In_Out.cpp
@@ -7,8 +7,12 @@ i. Правильный восьмиугольник
 int main()
 {
     int a, S;
+    
     std::cout << "Enter side length: ";
+    
     std::cin >> a;
+    
     std::cout << "S = " << 2 * a * a * (sqrt(2) + 1) << std::endl;
+    
     return 0;
 }
