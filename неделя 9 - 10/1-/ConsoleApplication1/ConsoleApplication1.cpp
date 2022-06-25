2080a67e964667666804a45743b13252012831c1
VakFretTref <93794794+VakFretTref@users.noreply.github.com>
2022-06-25 15:42:33 +0300

    .

--- неделя 9 - 10/1-/ConsoleApplication1/ConsoleApplication1.cpp
+++ неделя 9 - 10/1-/ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,7 +1,28 @@
-﻿#include <iostream>
-//1
+#include <iostream>
+
+void HIT(float X, float Y, float R, float r)
+{
+    if (X >= 0 && R && (Y <= 0 || r))
+        std::cout << "Hit";
+    else
+        std::cout << "loss";
+}
 
 int main()
 {
-	//что?
+    float x, y;
+
+    std::cout << "X = ";
+    std::cin >> x;
+
+    std::cout << "Y = ";
+    std::cin >> y;
+
+    float R1 = sqrt(pow(x, 2) + pow(y, 2)) <= 1;
+
+    float R2 = sqrt(pow(x, 2) + pow(y, 2)) >= 0.3;
+
+    HIT(x, y, R1, R2);
+
+    return 0;
 }
