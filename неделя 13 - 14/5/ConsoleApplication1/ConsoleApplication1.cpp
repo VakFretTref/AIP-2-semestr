a1543c2d273f7fe252da54ea0d8ba5f483e591b7
NikitaPerepelov <vakfret@gmail.com>
2022-06-25 15:56:10 +0300

    .

---
+++ неделя 13 - 14/5/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <array>
+//11   вариант 16
+
+int main()
+{
+	srand(5);
+
+	std::array<int, 20> M;
+
+	for (int i = 0; i < 20; i++)
+		M[i] = rand() % 21 - 10;
+
+	int quantity = M[0];
+
+	for (int i = 1; i < 20; i++)
+		quantity += M[i];
+
+	quantity /= 20;
+
+	std::cout << "arithmetic averages = ";
+
+	for (int i = 0; i < 20; i++)
+		if (M[i] < quantity)
+			std::cout << M[i] << ' ';
+
+	std::cout << std::endl;
+
+	return 0;
+}
---
+++ неделя 13 - 14/6/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <array>
+//14 b   вариант 16
+
+int main()
+{
+	srand(5);
+
+	std::array<int, 20> M1, M2, M3;
+
+	for (int i = 0; i < 20; i++)
+	{
+		M1[i] = rand() % 21 - 10;
+		M2[i] = rand() % 21 - 10;
+	}
+
+	for (int i = 0; i < 20; i++)
+		M3[i] = M2[i] - M1[i];
+
+	std::cout << "New array:\n";
+
+	for (int i = 0; i < 20; i++)
+			std::cout << M3[i] << ' ';
+
+	std::cout << std::endl;
+
+	return 0;
+}
